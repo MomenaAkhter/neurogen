f1d5356ea8a29d7156874cb13d6ce169e9da30d6
MomenaAkhter <makhter.shukhi@gmail.com>
2021-02-20 13:46:42 +0600

    Reorganize directory; add NEATPack.

---
+++ Assets/Extensions/UnityNEAT/Assets/SharpNEAT/UnitController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using SharpNeat.Phenomes;
+
+public abstract class UnitController : MonoBehaviour
+{
+    public bool IsRunning = false;
+    public abstract void Activate(IBlackBox box);
+
+    public abstract void Stop();
+
+    public abstract float GetFitness();
+}
