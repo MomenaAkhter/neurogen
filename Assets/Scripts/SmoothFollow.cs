3528fbbdea92aafa42cf916eb721795aef2cf75a
MomenaAkhter <makhter.shukhi@gmail.com>
2021-02-24 20:25:39 +0600

    Make camera follow car in perspective view.

--- Assets/Scripts/SmoothFollow.cs
+++ Assets/Scripts/SmoothFollow.cs
@@ -3,41 +3,44 @@ using System.Collections;
 
 public class SmoothFollow : MonoBehaviour
 {
-	public bool follow = true;
+    public bool follow = true;
 
-	// Target to follow.
-	public Transform target;
-	public float height = 20.0f;
-	public float smoothDampTime = 0.5f;
+    // Target to follow.
+    public Transform target;
+    public float height = 20.0f;
+    public float smoothDampTime = 0.5f;
 
-	// The point in at which the camera will be set in full view.
-	[SerializeField] private Transform allMapViewPosition = null;
+    // The point in at which the camera will be set in full view.
+    [SerializeField] private Transform allMapViewPosition = null;
 
-	private Vector3 smoothDampVel;
+    private Vector3 smoothDampVel;
 
-	void LateUpdate()
-	{
-		if (!target || !follow)
-			return;
-		SmoothDampToTarget();
-	}
+    void LateUpdate()
+    {
+        if (!target || !follow)
+            return;
+        SmoothDampToTarget();
+    }
 
-	void SmoothDampToTarget()
-	{
-		var targetPosition = target.position + Vector3.up * height;
-		transform.position = Vector3.SmoothDamp(
-			transform.position,
-			targetPosition,
-			ref smoothDampVel,
-			smoothDampTime
-		);
-	}
+    void SmoothDampToTarget()
+    {
+        var targetPosition = target.position + Vector3.up * height;
+        targetPosition = transform.TransformPoint(transform.InverseTransformPoint(targetPosition) - new Vector3(0, 0, 12));
+        transform.position = Vector3.SmoothDamp(
+            transform.position,
+            targetPosition,
+            ref smoothDampVel,
+            smoothDampTime
+        );
 
-	public void GotoAllMapView()
-	{
-		if (allMapViewPosition == null)
-			return;
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, target.rotation, 1);
+    }
 
-		transform.position = allMapViewPosition.position;
-	}
+    public void GotoAllMapView()
+    {
+        if (allMapViewPosition == null)
+            return;
+
+        transform.position = allMapViewPosition.position;
+    }
 }
