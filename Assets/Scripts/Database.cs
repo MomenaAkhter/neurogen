c40758a311c7a07c1887c26fe98de2d219a9ed97
MomenaAkhter <makhter.shukhi@gmail.com>
2021-03-08 22:43:35 +0600

    Update C# database class.

--- Assets/Scripts/Database.cs
+++ Assets/Scripts/Database.cs
@@ -1,24 +1,58 @@
 using System.Runtime.InteropServices;
+using System;
 
 namespace NeuroGen
 {
+    [StructLayout(LayoutKind.Sequential)]
+    public struct Model
+    {
+        public string content;
+        public int extension_id;
+        public float fitness;
+    }
+
     public class Database
     {
 #if UNITY_IPHONE
-   
-       // On iOS plugins are statically linked into
-       // the executable, so we have to use __Internal as the
-       // library name.
-       [DllImport ("__Internal")]
-
+        [DllImport("__Internal")]
 #else
-
-        // Other platforms load plugins dynamically, so pass the name
-        // of the plugin's dynamic library.
         [DllImport("Database")]
-
 #endif
-        public static extern int getSqliteVersion();
+        public static extern int GetSqliteVersion();
 
+#if UNITY_IPHONE
+        [DllImport("__Internal")]
+#else
+        [DllImport("Database")]
+#endif
+        public static extern int ConnectAndSetup(string path);
+
+#if UNITY_IPHONE
+        [DllImport("__Internal")]
+#else
+        [DllImport("Database")]
+#endif
+        public static extern int Disconnect();
+
+#if UNITY_IPHONE
+        [DllImport("__Internal")]
+#else
+        [DllImport("Database")]
+#endif
+        public static extern int AddModel(string content, string extensionName, float fitness);
+
+#if UNITY_IPHONE
+        [DllImport("__Internal", CallingConvention = CallingConvention.Cdecl)]
+#else
+        [DllImport("Database", CallingConvention = CallingConvention.Cdecl)]
+#endif
+        public static extern IntPtr GetModel(int id);
+
+#if UNITY_IPHONE
+        [DllImport("__Internal", CallingConvention = CallingConvention.Cdecl)]
+#else
+        [DllImport("Database", CallingConvention = CallingConvention.Cdecl)]
+#endif
+        public static extern void DeleteModel(IntPtr model);
     }
 }
